50401c48c06b698b3f34586b214d58fb0f254c9f
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-01-12 20:58:35 +0000

    Initial commit

---
+++ spec/calcSpec.js
@@ -0,0 +1,8 @@
+Description("Calculator", function(){
+    describe("Addition tests", function(){
+        it("should return 42", function(){
+            expect(addition(20, 22)).toBe(42);
+        });
+    });
+
+});
